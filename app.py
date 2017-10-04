f5d795e6e8be407e39ed37445a19408f81c14699
Yuyang Zhang <yzhang4@stuy.edu>
2017-10-04 18:36:39 -0400

    Noted errors

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, Session
 
 
 app = Flask(__name__)
 
+    
 @app.route("/")
-def welcome():
-    app.secret_key = "chicken"
-    if (request.args('username') == "ham" and
-        request.args('password') == app.secret_key):
+def welcome():   
         return render_template('welcome.html')
 
 @app.route("/logged_in") 
 def logged_in():
-    return render_template('logged_in.html', username = request.args['username']) 
+  Session['username'] = 'admin' #ERROR
+  app.secret_key = "password"
+  Session['password'] = app.secret_key #ERROR
+  if (request.args('username') == Session['username'] and request.args('password') == Session['password']):
+      return render_template('logged_in.html', username = request.args['username'])
 
 if __name__ == "__main__":
     app.debug = True
