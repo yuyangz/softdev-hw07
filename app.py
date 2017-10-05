1c0b07ebcb4255637ef385e6c0a2367a16ac1fe5
Yuyang Zhang <yzhang4@stuy.edu>
2017-10-04 23:28:44 -0400

    Page now loads

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template, request, Session
+from flask import Flask, render_template, request, session
 
 
 app = Flask(__name__)
-
+app.secret_key = "password"
     
 @app.route("/")
-def welcome():   
+def welcome():
+        session['username'] = "admin"
         return render_template('welcome.html')
 
 @app.route("/logged_in") 
 def logged_in():
-  Session['username'] = 'admin' #ERROR
-  app.secret_key = "password"
-  Session['password'] = app.secret_key #ERROR
-  if (request.args('username') == Session['username'] and request.args('password') == Session['password']):
-      return render_template('logged_in.html', username = request.args['username'])
+        if 'username' in session:
+                return render_template('logged_in.html', username = request.args['username'])
+        else:
+                return render_template('logged_in.html')
+        session.pop("username")
+
 
 if __name__ == "__main__":
     app.debug = True
